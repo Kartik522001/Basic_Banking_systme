563e745b2de3bf2aa0a526bbfec630d79adfb077
Kartik522001 <79997014+Kartik522001@users.noreply.github.com>
2021-09-18 21:50:50 +0530

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Indian Bank";
+
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+
---
+++ contactUs.php
@@ -0,0 +1,65 @@
+<?php
+  include 'navbar.php';
+?>
+
+<?php
+  include 'Config.php';
+?>
+<br>
+<br>
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    .fa {
+
+        padding: 20px;
+        font-size: 30px;
+        width: 65px;
+        text-align: center;
+        text-decoration: none;
+        margin: 5px 2px;
+        border-radius: 50%;
+    }
+
+    .fa:hover {
+        opacity: 0.7;
+    }
+
+
+    .fa-linkedin {
+        background: #007bb5;
+        color: white;
+    }
+
+
+    .fa-instagram {
+        background: #125688;
+        color: white;
+    }
+
+
+    }
+    </style>
+    <title>Contact Page</title>
+</head>
+
+<body>
+    <div class="container">
+        <div style="font-family: 'Courier New', monospace;">
+            <h2><strong>Contect us </strong></h2><br>
+            <h5>-Telephone: +(91) 9974390706</h5>
+            <h5>-Email: panchalkartik30@gmail.com</h5>
+        </div>
+        <br>
+        <!-- Add font awesome icons -->
+        <a href="https://www.linkedin.com/in/kartik-panchal-2848a716a/" class="fa fa-linkedin"></a>
+        <a href="https://www.instagram.com/panchal_kartik_30/" class="fa fa-instagram"></a>
+
+
+</body>
+
+</html>
