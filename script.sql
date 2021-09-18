3d352b508d2c95271abf438596e912b9d23e3a72
Kartik522001 <79997014+Kartik522001@users.noreply.github.com>
2021-09-18 21:58:05 +0530

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,22 @@
+create table transaction
+(
+    sno      int(3) auto_increment
+        primary key,
+    sender   text                                 not null,
+    receiver text                                 not null,
+    balance  int(8)                               not null,
+    datetime datetime default current_timestamp() not null
+)
+    collate = utf8_bin;
+
+create table users
+(
+    id      int(3) auto_increment
+        primary key,
+    name    text        not null,
+    email   varchar(30) not null,
+    balance int(8)      not null
+)
+    collate = utf8_bin;
+
+
